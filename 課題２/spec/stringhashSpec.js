6f88f3459ef19a3efa300e15ae8b0a354da99c24
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-05 21:48:55 +0900

    stringHash html

--- 課題２/spec/stringhashSpec.js
+++ 課題２/spec/stringhashSpec.js
@@ -1,6 +1,6 @@
 describe("StringHash", function () {
 
-    it("no js no life", function () {
+    it("count no js no life", function () {
         
         let h = stringHash("no js no life");
         expect(h["no"]).toEqual(2);
@@ -9,7 +9,7 @@ describe("StringHash", function () {
         expect(Object.keys(h).length).toEqual(3);
     });
 
-    it("no js no life no test no test", function () {
+    it("coount no js no life no test no test", function () {
 
         let h = stringHash("no js no life no test no test");
         expect(h["no"]).toEqual(4);
@@ -18,5 +18,4 @@ describe("StringHash", function () {
         expect(h["test"]).toEqual(2);
         expect(Object.keys(h).length).toEqual(4);
     });
-
 });
--- 課題２/src/stringhash.js
+++ 課題２/src/stringhash.js
@@ -1,15 +1,19 @@
 function stringHash(s){
     let h = {};
-
     let elemnt = s.split(" ")
-
     elemnt.forEach(str => {
-        if (h[str]){
-            h[str] += 1
-        }else{
-            h[str] = 1
-        }
+        h[str] = h[str]? h[str]+1 : 1 
     });
-
     return h;
+}
+
+function stringhashExe() {
+    let h = {}
+    let str = document.getElementById("input_val").value;
+    h = stringHash(str)
+    for (let key in h) {
+        let item = document.createElement("li");
+        item.textContent = key + ":" + h[key]
+        container.appendChild(item)
+    }
 }
