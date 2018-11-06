2b76c9111761fdce0272ece9eaed9b863c5f9cd2
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-06 22:42:57 +0900

    toUry temporary

--- 課題８/spec/covertHash.js
+++ 課題８/spec/covertHash.js
@@ -1,9 +1,22 @@
 function toUry(array){
-    hash = {
-        "main": {
+ 
+    hash ={
+        "main":
+        {
             "first": { "text": "uryyyy!!bar" },
             "second": { "text": "fizzbuzz", "child": { "text": "uryyyy!!bar" } }
         },
+        "sub":
+        {
+            "first": { "text": "fizzbuzz", "child": { "text": "uryyyy!!bar" } },
+            "second":
+            {
+                "third": { "text": "baruryyyy!!", "child": { "text": "uryyyy!!bar" } },
+                "forth": { "child": { "text": "jit_uryyyy!!_uryyyy!!" } }
+            }
+        },
+        "text": "uryyyy!!fava"
     }
+
     return hash;
 }
--- 課題８/src/convertHashSpec.js
+++ 課題８/src/convertHashSpec.js
@@ -1,15 +1,30 @@
 describe("convertHash", function () {
 
-    it("bubble sort sample1", function () {
+    it("toUry", function () {
         hash = {
             "main": {
                 "first": { "text": "foobar" },
                 "second": { "text": "fizzbuzz", "child": { "text": "foobar" } }
             },
+            "sub" : {
+                "first": { "text": "fizzbuzz", "child": { "text": "foobar" } },
+                "second": {
+                    "third": { "text": "barfoo", "child": { "text": "foobar" } },
+                    "forth": { "child": { "text": "jit_foo_foo" } }
+                }
+            },
+            "text" : "foofava"
         }
         result = toUry(hash);
 
         expect(result.main.first.text).toEqual("uryyyy!!bar");
+        expect(result.main.second.text).toEqual("fizzbuzz");
         expect(result.main.second.child.text).toEqual("uryyyy!!bar");
+        expect(result.sub.first.text).toEqual("fizzbuzz");
+        expect(result.sub.first.child.text).toEqual("uryyyy!!bar");
+        expect(result.sub.second.third.text).toEqual("baruryyyy!!");
+        expect(result.sub.second.third.child.text).toEqual("uryyyy!!bar");
+        expect(result.sub.second.forth.child.text).toEqual("jit_uryyyy!!_uryyyy!!");
+        expect(result.text).toEqual("uryyyy!!fava");
     });
 });
