35d0403b98a911c86f53b48bf78cc88ab1e8fa8f
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-06 16:13:30 +0900

    Finis Hi&Low

---
+++ 課題６/src/hi_low.js
@@ -0,0 +1,32 @@
+let hilow;
+
+function init(){
+    hilow = new HiLow();
+}
+
+function hi_lowExe(){
+    let input = parseInt(document.getElementById("input_val").value);
+    let container = document.getElementById("container");
+    let item = document.createElement("li");
+
+    item.textContent = hilow.hi_low(parseInt(input));
+    container.appendChild(item)
+}
+
+class HiLow{
+    constructor(){
+        this.target = Math.floor(Math.random() * 100);
+        this.trycount = 0;
+    }
+
+    hi_low(number){
+        this.trycount++;
+        if(number == this.target){
+            return "あたり:"+number+" -> 試行回数（"+this.trycount+"）";
+        } else if (number > this.target) {
+            return "もっと下:"+number;
+        }else{
+            return "もっと上:" + number;
+        }
+    }
+}
