6bf1d257c876656ee8c5878d34a3453d0d5bbe06
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-06 11:52:51 +0900

    Finish task4

--- 課題４/src/recursion.js
+++ 課題４/src/recursion.js
@@ -5,3 +5,9 @@ function recursion(input){
     }
     return input + recursion(input-1)
 }
+
+function recursionExe() {
+
+    let input = document.getElementById("input_val").value;
+    document.getElementById("result").textContent = recursion(parseInt(input));
+}
