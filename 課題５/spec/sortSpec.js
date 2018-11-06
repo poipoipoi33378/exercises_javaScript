27503a9010d1cc039a7d09b02ddadcfc1ff41422
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-06 14:00:37 +0900

    start quick sort

--- 課題５/spec/sortSpec.js
+++ 課題５/spec/sortSpec.js
@@ -1,6 +1,6 @@
 describe("sort", function () {
 
-    it("bubble sort", function () {
+    it("bubble sort sample1", function () {
         let arry = [20,31,42,5,38];
 
         let result = bubbleSort(arry);
@@ -10,4 +10,44 @@ describe("sort", function () {
         expect(result[3]).toEqual(38)
         expect(result[4]).toEqual(42)
     });
+
+    it("bubble sort sample2", function () {
+        let arry = [20, 31, 42, 5, 38,0,-1,100];
+
+        let result = bubbleSort(arry);
+        expect(result[0]).toEqual(-1)
+        expect(result[1]).toEqual(0)
+        expect(result[2]).toEqual(5)
+        expect(result[3]).toEqual(20)
+        expect(result[4]).toEqual(31)
+        expect(result[5]).toEqual(38)
+        expect(result[6]).toEqual(42)
+        expect(result[7]).toEqual(100)
+    });
+
+    it("bubble sort sample3", function () {
+        let arry = [20, 31, 42, 5, 38, 38, 31, 100];
+
+        let result = bubbleSort(arry);
+        expect(result[0]).toEqual(5)
+        expect(result[1]).toEqual(20)
+        expect(result[2]).toEqual(31)
+        expect(result[3]).toEqual(31)
+        expect(result[4]).toEqual(38)
+        expect(result[5]).toEqual(38)
+        expect(result[6]).toEqual(42)
+        expect(result[7]).toEqual(100)
+    });
+
+    it("quick sort sample2", function () {
+        let arry = [20, 31, 42, 5, 38];
+
+        let result = quickSort(arry);
+        expect(result[0]).toEqual(5)
+        expect(result[1]).toEqual(20)
+        expect(result[2]).toEqual(31)
+        expect(result[3]).toEqual(38)
+        expect(result[4]).toEqual(42)
+    });
+
 });
--- 課題５/src/sort.js
+++ 課題５/src/sort.js
@@ -1,3 +1,31 @@
-function bubbleSort(array){
-    return [5,20,31,38,42];
+function bubbleSort(input){
+    
+    let target;
+    let array = input.slice(0, input.length);
+
+    for(let i=0;i<array.length-1;i++){
+        for (let j = 0; j < array.length-1-i;j++){
+            if(array[j] > array[j+1]){
+                let tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+            target = array[j + 1];
+        }
+    }
+    return array;
+}
+
+function quickSort(input) {
+
+    let pivot = 20;
+    let left = [5];
+    let right = [31,38,42];
+
+    let result = [];
+    result = result.concat(left);
+    result = result.concat(pivot);
+    result = result.concat(right);
+
+    return result;
 }
