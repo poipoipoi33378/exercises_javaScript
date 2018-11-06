51e0ea00fd1becdea20263718b18dbc3bceedce3
Seiichi Sugahara <poipoipoi33378@gmail.com>
2018-11-06 21:33:24 +0900

    modify test and code

--- 課題７/spec/hit_blowSpec.js
+++ 課題７/spec/hit_blowSpec.js
@@ -1,14 +1,56 @@
-describe("Hit&Blow", function () {
+class GenerateTestNumber {
+    constructor(){
+        this.dumyArray = [];
+        this.dumycounter = 0;
+    }
 
-    it("create target", function () {
+    number0to9() {
+        let num = this.dumyArray[this.dumycounter];
+        this.dumycounter++;
+        return num;
+    }
+}
+
+describe("HitAndBlow", function () {
+
+    it("create target from [1,2,3,9]", function () {
+        let dumy = new GenerateTestNumber();
+        dumy.dumyArray = [1, 2, 3, 9];
+        let hb = new HitAndBlow(dumy);
+
+        expect(hb.target[0]).toEqual(1);
+        expect(hb.target[1]).toEqual(2);
+        expect(hb.target[2]).toEqual(3);
+        expect(hb.target[3]).toEqual(9);
+    });
+
+    it("create target from [9,9.9,9]", function () {
+        let dumy = new GenerateTestNumber();
+        dumy.dumyArray = [9, 9, 9, 9];
+        let hb = new HitAndBlow(dumy);
+
+        expect(hb.target[0]).toEqual(9);
+        expect(hb.target[1]).toEqual(1);
+        expect(hb.target[2]).toEqual(2);
+        expect(hb.target[3]).toEqual(3);
+    });
+
+    it("create from [1,1.9,9]", function () {
+        let dumy = new GenerateTestNumber();
+        dumy.dumyArray = [1, 1, 9, 9];
+        let hb = new HitAndBlow(dumy);
+
+        expect(hb.target[0]).toEqual(1);
+        expect(hb.target[1]).toEqual(2);
+        expect(hb.target[2]).toEqual(9);
+        expect(hb.target[3]).toEqual(3);
+    });
+
+    it("create target from random", function () {
         let hb = new HitAndBlow();
 
         for(let i=0;i<4;i++){
-            for (let j = 0; j < 4; j++) {
-                if(i!=j){
-                    expect(hb.target[i]).not.toEqual(hb.target[j]);
-                }
-            }
+            expect((hb.target[i] >= 0) && (hb.target[i] <= 9)).toEqual(true);
         }
     });
 
@@ -30,6 +72,16 @@ describe("Hit&Blow", function () {
 
         expect(hb.result.hit).toEqual(0);
         expect(hb.result.blow).toEqual(4);
+    });
+
+    it("tryCode 2hit 2blow", function () {
+        let hb = new HitAndBlow();
+
+        hb.target = [1, 2, 3, 4];
+        hb.tryCode([1, 4, 3, 2]);
+
+        expect(hb.result.hit).toEqual(2);
+        expect(hb.result.blow).toEqual(2);
     });
 
 });
--- 課題７/src/hit_blow.js
+++ 課題７/src/hit_blow.js
@@ -1,19 +1,29 @@
+class GenerateRandom{
+    number0to9() {
+        return Math.floor(Math.random() * 10)
+    }
+}
 class HitAndBlow{
-    constructor(){
+    constructor(nextNumber=null){
+        if(nextNumber == null){
+            this.nextNumber = new GenerateRandom();
+        }else{
+            this.nextNumber = nextNumber;
+        }
+        this.result = {hit:0,blow:0};
         this.target = [];
         this.setTarget();
-        this.result = {hit:0,blow:0};
     }
 
     setTarget(){
-        this.target.push(Math.floor(Math.random() * 10));
+        this.target.push(this.nextNumber.number0to9());
         for (let i = 0; i < 3; i++) {
-            let num = Math.floor(Math.random() * 10);
+            let num = this.nextNumber.number0to9();
             for (let j = 0; j < 4; j++) {
                 if (this.target.indexOf(num) < 0) {
                     break;
                 }
-                num++;
+                num = (num+1)%9;
             }
             this.target.push(num);
         }
